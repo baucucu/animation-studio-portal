55ae4334a3f0a19c0e59f4809e8a349ca54b1a47
baucucu <alexandru.raduca@gmail.com>
2021-09-29 12:45:59 +0300

    update

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,24 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import './profile.scss';
 import Form from 'devextreme-react/form';
 import { useAuth } from '../../contexts/auth';
 
 
 export default function Profile() {
-  // const { user, signOut } = useAuth();
   const { user } = useAuth();
-  const [notes, setNotes] = useState(
-    'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.'
-  );
   const employee = {
     // ID: user.id,
     Name: user.customData?.name,
     Email: user._profile.data.email,
     Role: user.customData?.role,
     Company: user.customData?.company
-    // Picture: user.customData?.avatarUrl,
-    // Notes: notes,
-    // Address: '4600 N Virginia Rd.'
   };
 
   useEffect(() => {
@@ -37,25 +30,14 @@ export default function Profile() {
             src={user.customData.avatarUrl}
           />
         </div>
-        {/* <span>{notes}</span> */}
         <span><Form
           id={'form'}
           defaultFormData={employee}
-          onFieldDataChanged={e => e.dataField === 'Notes' && setNotes(e.value)}
+          // onFieldDataChanged={e => e.dataField === 'Notes' && setNotes(e.value)}
           labelLocation={'top'}
           colCountByScreen={colCountByScreen}
         /></span>
       </div>
-
-      {/* <div className={'content-block dx-card responsive-paddings'}>
-        <Form
-          id={'form'}
-          defaultFormData={employee}
-          onFieldDataChanged={e => e.dataField === 'Notes' && setNotes(e.value)}
-          labelLocation={'top'}
-          colCountByScreen={colCountByScreen}
-        />
-      </div> */}
     </React.Fragment>
   );
 }
