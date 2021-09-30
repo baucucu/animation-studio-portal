edb9651afade640603f9a1e7cda0ef859c6f3d95
baucucu <alexandru.raduca@gmail.com>
2021-09-30 16:41:36 +0300

    added tabs to project details

--- src/pages/project/project.js
+++ src/pages/project/project.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import * as Realm from "realm-web";
 import Button from 'devextreme-react/button';
+import Tabs from 'devextreme-react/tabs';
 import { useHistory } from "react-router-dom";
 import './project.scss';
 const BSON = require('bson');
@@ -13,9 +14,23 @@ const projectsColection = mongodb.db("AnimationStudioDB").collection("Projects")
 export default function Project() {
   const history = useHistory();
   const projectId = history.location.pathname.split(":")[1]
-  console.log("mongoId: ", projectId)  
+  const tabs = [
+    { text: 'Brief','icon':'user', index:0 },
+    { text: 'Manuscript','icon':'user', index:1 },
+    { text: 'Storyboard','icon':'user', index:2 },
+    { text: 'Voiceover','icon':'user', index:3  },
+    { text: 'Illustrations','icon':'user', index:4  },
+    { text: 'Animation','icon':'user', index:5  },
+    { text: 'Delivery','icon':'user', index:6  },
+  ];
 
   const [project, setProject] = useState()
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  function onTabIndexChanged (e) {
+    // console.log("event: ", e)
+    e.name === 'selectedItem' && setSelectedIndex(e.value.index)
+  }
 
   async function getProject(projectId){
     return await projectsColection.find({_id: BSON.ObjectId(projectId)})
@@ -37,30 +52,38 @@ export default function Project() {
     <React.Fragment>
       <Button icon="back" onClick={history.goBack} style={{margin:8}}></Button>
       <h2 className={'content-block'}>Project</h2>
-      {project && <div className={'content-block'}>
-        <div className={'dx-card responsive-paddings'}>
-          <div className="project">
-          <div className="mongoId">MongoDB id: {project._id.toString()}</div>
-          <div className="projectName">Deal: {project.projectName}</div>
-          <div className="ownerName">Project manager: {`${project.projectOwnerName}`}</div>
-          <div className="clientName">Client contact name: {`${project.clientName}`}</div>
-          <div className="clientName">Client contact email: {`${project.clientEmail}`}</div>
-          <div className="proposalUrl">Proposal URL: {`${project.proposalURL}`}</div>
-          <div className="firstWonTime">Won time: {`${project.firstWonTime}`}</div>
-          <div className="dealValue">Deal value: {`${project.dealValue} ${project.currency}`}</div>
-          <div className="products">
-            <b>Products: </b>
-            {project.products.map((product, id) => {
-              return (
-                <div key={id}>
-                  <div className="product">{product.quantity} x {product.name} @ {product.sum_formatted} </div>
-                </div>
-              )
-            })}
+      {project && <>
+        <div className={'content-block'}>
+          <div className={'dx-card responsive-paddings'}>
+            <div className="project">
+            <div className="mongoId">MongoDB id: {project._id.toString()}</div>
+            <div className="projectName">Deal: {project.projectName}</div>
+            <div className="ownerName">Project manager: {`${project.projectOwnerName}`}</div>
+            <div className="clientName">Client contact name: {`${project.clientName}`}</div>
+            <div className="clientName">Client contact email: {`${project.clientEmail}`}</div>
+            <div className="proposalUrl">Proposal URL: {`${project.proposalURL}`}</div>
+            <div className="firstWonTime">Won time: {`${project.firstWonTime}`}</div>
+            <div className="dealValue">Deal value: {`${project.dealValue} ${project.currency}`}</div>
+            <div className="products">
+              <b>Products: </b>
+              {project.products.map((product, id) => {
+                return (
+                  <div key={id}>
+                    <div className="product">{product.quantity} x {product.name} @ {product.sum_formatted} </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
           </div>
         </div>
-        </div>
-      </div>}
+        <Tabs
+          dataSource={tabs}
+          selectedIndex={selectedIndex}
+          onOptionChanged={onTabIndexChanged}
+        >
+        </Tabs>
+      </>}
     </React.Fragment>
   )
 };
